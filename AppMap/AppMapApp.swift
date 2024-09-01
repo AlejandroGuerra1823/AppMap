34e6b2af7ff39dcfbe834e3cdce831ff738d14c0
Alejandro Guerra <guerramanuel299@gmail.com>
2024-09-01 14:54:20 -0500

    Initial Commit

---
+++ AppMap/AppMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMapApp.swift
+//  AppMap
+//
+//  Created by Alejandro Guerra on 1/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AppMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
